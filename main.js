47df56b33b864f8385260daef05bf824577f7272
Olivier Monnier <omonnier.dev@gmail.com>
2016-07-08 14:43:56 +0200

    add translate by click maintained

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var rotate = 100;
 var pageX = $(document).width();
 var pageY = $(document).height();
 
-function applyCssTransform($el, rX, rY, scale) {
-  $el.css('-webkit-transform', 'rotateY(' + rY + 'deg) rotateX(' + rX + 'deg) scale(' + scale + ')');
+function applyCssTransform($el, rX, rY, tX, tY, scale) {
+  $el.css('transform', 'rotateY(' + rY + 'deg) rotateX(' + rX + 'deg) translate(' + tX + 'px, ' + tY + 'px) scale(' + scale + ')');
 }
 
 function initSceneView() {
@@ -13,31 +13,41 @@ function initSceneView() {
     var mouseY = 0;
     var rotateX = 0;
     var rotateY = 0;
+    var translateX = 0;
+    var translateY = 0;
     var scale = 1
+    var mouseDownStarted = [];
 
-    $scene.find('.scene-content').css({
-      'position': 'relative',
-      'perspective': '1000px',
-      'perspective-origin': 'center center',
-      '-webkit-transform-style': 'preserve-3d'
+    $scene.on('mousedown', function( event ) {
+      mouseDownStarted = [event.pageX - translateX, event.pageY - translateY];
     });
 
-    $scene.bind('mousemove', function( event ) {
-      mouseX = event.pageY;
-      mouseY = event.pageX;
-      rotateY = -(pageY / 2 - mouseY) / pageY * rotate;
-      rotateX = (pageX/2 - mouseX) / pageX * rotate;
-     });
+    $scene.on('mouseup', function( event ) {
+      $scene.bind('mousedown')
+    });
 
-    $scene.bind('mousewheel', function( event ) {
+    $scene.on('mousemove', function( event ) {
+      if (event.buttons == 1) {
+        event.preventDefault();
+        translateX = event.pageX - mouseDownStarted[0];
+        translateY = event.pageY - mouseDownStarted[1];
+      } else {
+        mouseX = event.pageY;
+        mouseY = event.pageX;
+        rotateY = -(pageY / 2 - mouseY) / pageY * rotate;
+        rotateX = (pageX/2 - mouseX) / pageX * rotate;
+      }
+    });
+
+    $scene.on('mousewheel', function( event ) {
       var n = (event.originalEvent.wheelDelta /120 > 0) ? .1 : -.1;
       scale = scale + n;
     });
 
-    $scene.bind('mousemove mousewheel', function() {
+    $scene.on('mousemove mousewheel', function() {
       var $content = $(this).find('.scene-content');
 
-      applyCssTransform($content, rotateX, rotateY, scale);
+      applyCssTransform($content, rotateX, rotateY, translateX, translateY, scale);
     });
   });
 }
